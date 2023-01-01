bc8d5883b968214e890a966815c71b2e44c2bb9e
KallDrexx <me@mshapiro.net>
2022-12-31 20:59:53 -0500

    Removed audio and video codec enums that are no longer used

--- mmids-core/src/codecs.rs
+++ mmids-core/src/codecs.rs
@@ -6,17 +6,3 @@ lazy_static! {
     pub static ref VIDEO_CODEC_H264_AVC: Arc<String> = Arc::new("h264-avc".to_string());
     pub static ref AUDIO_CODEC_AAC_RAW: Arc<String> = Arc::new("aac-raw".to_string());
 }
-
-/// Audio codecs that can be identified
-#[derive(Debug, Clone, Eq, PartialEq, Copy)]
-pub enum AudioCodec {
-    Unknown,
-    Aac,
-}
-
-/// Video codecs that can be identified
-#[derive(Debug, Clone, Eq, PartialEq, Copy)]
-pub enum VideoCodec {
-    Unknown,
-    H264,
-}
